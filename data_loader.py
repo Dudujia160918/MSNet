563aca2744edeb4596b1c1b1816b5d65a6bc826d
DY <33213847+Dudujia160918@users.noreply.github.com>
2023-03-25 14:03:06 +0800

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,276 @@
+# data loader
+from __future__ import print_function, division
+
+import copy
+import glob
+import torch
+from skimage import io, transform, color
+import numpy as np
+import random
+import math
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from einops import rearrange
+from skimage.feature import local_binary_pattern
+import cv2
+from torchvision import transforms
+#==========================dataset load==========================
+class RescaleT(object):
+
+	def __init__(self,output_size):
+		assert isinstance(output_size,(int,tuple))
+		self.output_size = output_size
+
+	def __call__(self,sample):
+		#############################################LBP不裁剪版本####################################################
+		imidx, image, label, LBP = sample['imidx'], sample['image'], sample['label'],sample['LBP']
+		img = transform.resize(image,(self.output_size,self.output_size),mode='constant')
+		LBP = transform.resize(LBP,(self.output_size/4,self.output_size/4),mode='constant')
+		lbl = transform.resize(label,(self.output_size,self.output_size),mode='constant', order=0, preserve_range=True)
+
+		return {'imidx': imidx, 'image': img, 'label': lbl, 'LBP': LBP}
+class RandomCrop(object):
+
+	def __init__(self,output_size):
+		assert isinstance(output_size, (int, tuple))
+		if isinstance(output_size, int):
+			self.output_size = (output_size, output_size)
+		else:
+			assert len(output_size) == 2
+			self.output_size = output_size
+	def __call__(self,sample):
+		imidx, image, label,LBP = sample['imidx'], sample['image'], sample['label'],sample["LBP"]
+
+		if random.random() >= 0.5:
+			image = image[::-1]
+			label = label[::-1]
+			LBP = LBP[::-1]
+		LBP = cv2.resize(LBP, (128, 128), interpolation=cv2.INTER_CUBIC)
+		h, w = LBP.shape[:2]
+		new_h, new_w = self.output_size
+
+		top = random.randrange(0, h - new_h,4)
+		left = random.randrange(0, w - new_w,4)
+
+		#image = image[top: top + new_h, left: left + new_w]
+		#label = label[top: top + new_h, left: left + new_w]
+		LBP = LBP[top: top + new_h, left: left + new_w]
+
+		return {'imidx':imidx,'image':image, 'label':label,"LBP":LBP}
+class SalObjDataset(Dataset):
+	def __init__(self,img_name_list_pan,tra_img_name_list_mul,lbl_name_list,transform=None, test=False):
+		self.image_name_list = img_name_list_pan
+		self.image_name_list_mul = tra_img_name_list_mul
+		self.label_name_list = lbl_name_list
+		self.transform = transform
+		self.test = test
+
+	def __len__(self):
+		return len(self.image_name_list)
+
+	def __getitem__(self,idx):
+		temp = np.expand_dims(cv2.imread(self.image_name_list[idx],0),axis=2)
+		image = io.imread(self.image_name_list[idx])
+		# if self.test:
+		# 	image = np.pad(image, (0, 112), 'constant')
+
+		LBP = io.imread(self.image_name_list_mul[idx])
+
+
+		imidx = np.array([idx])
+		if(0==len(self.label_name_list)):
+			label_3 = np.zeros(image.shape)
+		else:
+			label_3 = io.imread(self.label_name_list[idx])
+
+		label = np.zeros(label_3.shape[0:2])
+		if(3==len(label_3.shape)):
+			label = label_3[:,:,0]
+		elif(2==len(label_3.shape)):
+			label = label_3
+
+		if(3==len(image.shape) and 2==len(label.shape)and 3==len(LBP.shape)):
+			label = label[:,:,np.newaxis]
+
+		elif(2==len(image.shape) and 2==len(LBP.shape) and 2==len(label.shape)):
+			image = image[:,:,np.newaxis]
+			LBP = LBP[:, :, np.newaxis]
+			label = label[:,:,np.newaxis]
+
+		sample = {'imidx':imidx, 'image':image, "LBP":LBP, 'label':label}
+
+		if self.transform:
+			sample = self.transform(sample)
+
+		return sample
+
+class Rescale(object):
+
+	def __init__(self,output_size):
+		assert isinstance(output_size,(int,tuple))
+		self.output_size = output_size
+
+	def __call__(self,sample):
+		imidx, image, label = sample['imidx'], sample['image'],sample['label']
+
+		if random.random() >= 0.5:
+			image = image[::-1]
+			label = label[::-1]
+
+		h, w = image.shape[:2]
+
+		if isinstance(self.output_size,int):
+			if h > w:
+				new_h, new_w = self.output_size*h/w,self.output_size
+			else:
+				new_h, new_w = self.output_size,self.output_size*w/h
+		else:
+			new_h, new_w = self.output_size
+
+		new_h, new_w = int(new_h), int(new_w)
+
+		# #resize the image to new_h x new_w and convert image from range [0,255] to [0,1]
+		img = transform.resize(image,(new_h,new_w),mode='constant')
+		lbl = transform.resize(label,(new_h,new_w),mode='constant', order=0, preserve_range=True)
+
+		return {'imidx':imidx, 'image':img,'label':lbl}
+
+
+
+class ToTensor(object):
+	"""Convert ndarrays in sample to Tensors."""
+
+	def __call__(self, sample):
+
+		imidx, image, label = sample['imidx'], sample['image'], sample['label']
+
+		tmpImg = np.zeros((image.shape[0],image.shape[1],3))
+		tmpLbl = np.zeros(label.shape)
+
+		image = image/np.max(image)
+		if(np.max(label)<1e-6):
+			label = label
+		else:
+			label = label/np.max(label)
+
+		if image.shape[2]==1:
+			tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
+			tmpImg[:,:,1] = (image[:,:,0]-0.485)/0.229
+			tmpImg[:,:,2] = (image[:,:,0]-0.485)/0.229
+		else:
+			tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
+			tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
+			tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
+
+		tmpLbl[:,:,0] = label[:,:,0]
+
+		# change the r,g,b to b,r,g from [0,255] to [0,1]
+		#transforms.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))
+		tmpImg = tmpImg.transpose((2, 0, 1))
+		tmpLbl = label.transpose((2, 0, 1))
+
+		return {'imidx':torch.from_numpy(imidx).copy(), 'image': torch.from_numpy(tmpImg).copy(), 'label': torch.from_numpy(tmpLbl).copy()}
+
+class ToTensorLab(object):
+	"""Convert ndarrays in sample to Tensors."""
+	def __init__(self,flag=0):
+		self.flag = flag
+
+	def __call__(self, sample):
+
+		imidx, image, label, LBP = sample['imidx'], sample['image'], sample['label'], sample['LBP']
+
+		tmpLbl = np.zeros(label.shape)
+
+		if(np.max(label)<1e-6):
+			label = label
+		else:
+			label = label/np.max(label)
+
+		# change the color space
+		if self.flag == 2: # with rgb and Lab colors
+			tmpImg = np.zeros((image.shape[0],image.shape[1],6))
+			tmpImgt = np.zeros((image.shape[0],image.shape[1],3))
+			if image.shape[2]==1:
+				tmpImgt[:,:,0] = image[:,:,0]
+				tmpImgt[:,:,1] = image[:,:,0]
+				tmpImgt[:,:,2] = image[:,:,0]
+			else:
+				tmpImgt = image
+			tmpImgtl = color.rgb2lab(tmpImgt)
+
+			# nomalize image to range [0,1]
+			tmpImg[:,:,0] = (tmpImgt[:,:,0]-np.min(tmpImgt[:,:,0]))/(np.max(tmpImgt[:,:,0])-np.min(tmpImgt[:,:,0]))
+			tmpImg[:,:,1] = (tmpImgt[:,:,1]-np.min(tmpImgt[:,:,1]))/(np.max(tmpImgt[:,:,1])-np.min(tmpImgt[:,:,1]))
+			tmpImg[:,:,2] = (tmpImgt[:,:,2]-np.min(tmpImgt[:,:,2]))/(np.max(tmpImgt[:,:,2])-np.min(tmpImgt[:,:,2]))
+			tmpImg[:,:,3] = (tmpImgtl[:,:,0]-np.min(tmpImgtl[:,:,0]))/(np.max(tmpImgtl[:,:,0])-np.min(tmpImgtl[:,:,0]))
+			tmpImg[:,:,4] = (tmpImgtl[:,:,1]-np.min(tmpImgtl[:,:,1]))/(np.max(tmpImgtl[:,:,1])-np.min(tmpImgtl[:,:,1]))
+			tmpImg[:,:,5] = (tmpImgtl[:,:,2]-np.min(tmpImgtl[:,:,2]))/(np.max(tmpImgtl[:,:,2])-np.min(tmpImgtl[:,:,2]))
+
+			# tmpImg = tmpImg/(np.max(tmpImg)-np.min(tmpImg))
+
+			tmpImg[:,:,0] = (tmpImg[:,:,0]-np.mean(tmpImg[:,:,0]))/np.std(tmpImg[:,:,0])
+			tmpImg[:,:,1] = (tmpImg[:,:,1]-np.mean(tmpImg[:,:,1]))/np.std(tmpImg[:,:,1])
+			tmpImg[:,:,2] = (tmpImg[:,:,2]-np.mean(tmpImg[:,:,2]))/np.std(tmpImg[:,:,2])
+			tmpImg[:,:,3] = (tmpImg[:,:,3]-np.mean(tmpImg[:,:,3]))/np.std(tmpImg[:,:,3])
+			tmpImg[:,:,4] = (tmpImg[:,:,4]-np.mean(tmpImg[:,:,4]))/np.std(tmpImg[:,:,4])
+			tmpImg[:,:,5] = (tmpImg[:,:,5]-np.mean(tmpImg[:,:,5]))/np.std(tmpImg[:,:,5])
+
+		elif self.flag == 1: #with Lab color
+			tmpImg = np.zeros((image.shape[0],image.shape[1],3))
+
+			if image.shape[2]==1:
+				tmpImg[:,:,0] = image[:,:,0]
+				tmpImg[:,:,1] = image[:,:,0]
+				tmpImg[:,:,2] = image[:,:,0]
+			else:
+				tmpImg = image
+
+			tmpImg = color.rgb2lab(tmpImg)
+
+			# tmpImg = tmpImg/(np.max(tmpImg)-np.min(tmpImg))
+
+			tmpImg[:,:,0] = (tmpImg[:,:,0]-np.min(tmpImg[:,:,0]))/(np.max(tmpImg[:,:,0])-np.min(tmpImg[:,:,0]))
+			tmpImg[:,:,1] = (tmpImg[:,:,1]-np.min(tmpImg[:,:,1]))/(np.max(tmpImg[:,:,1])-np.min(tmpImg[:,:,1]))
+			tmpImg[:,:,2] = (tmpImg[:,:,2]-np.min(tmpImg[:,:,2]))/(np.max(tmpImg[:,:,2])-np.min(tmpImg[:,:,2]))
+
+			tmpImg[:,:,0] = (tmpImg[:,:,0]-np.mean(tmpImg[:,:,0]))/np.std(tmpImg[:,:,0])
+			tmpImg[:,:,1] = (tmpImg[:,:,1]-np.mean(tmpImg[:,:,1]))/np.std(tmpImg[:,:,1])
+			tmpImg[:,:,2] = (tmpImg[:,:,2]-np.mean(tmpImg[:,:,2]))/np.std(tmpImg[:,:,2])
+
+		else: # with rgb color
+			tmpImg = np.zeros((image.shape[0],image.shape[1],3))
+			image = image/np.max(image)
+			if image.shape[2]==1:
+				tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
+				tmpImg[:,:,1] = (image[:,:,0]-0.485)/0.229
+				tmpImg[:,:,2] = (image[:,:,0]-0.485)/0.229
+			else:
+				tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
+				tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
+				tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
+
+			tmpImg2 = np.zeros((LBP.shape[0], LBP.shape[1], 3))
+			LBP = LBP / np.max(LBP)
+			if LBP.shape[2] == 1:
+				tmpImg2[:, :, 0] = (LBP[:, :, 0] - 0.485) / 0.229
+				tmpImg2[:, :, 1] = (LBP[:, :, 0] - 0.485) / 0.229
+				tmpImg2[:, :, 2] = (LBP[:, :, 0] - 0.485) / 0.229
+			else:
+				tmpImg2[:, :, 0] = (LBP[:, :, 0] - 0.485) / 0.229
+				tmpImg2[:, :, 1] = (LBP[:, :, 1] - 0.456) / 0.224
+				tmpImg2[:, :, 2] = (LBP[:, :, 2] - 0.406) / 0.225
+
+		tmpLbl[:,:,0] = label[:,:,0]
+
+		# change the r,g,b to b,r,g from [0,255] to [0,1]
+		#transforms.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))
+		# tmpImg = tmpImg.transpose((2, 0, 1))
+		# tmpImg2 = tmpImg2.transpose((2, 0, 1))
+		# tmpLbl = label.transpose((2, 0, 1))
+		imidx = imidx.copy()
+		tmpImg = transforms.ToTensor()(tmpImg.copy())
+		tmpImg2 = transforms.ToTensor()(tmpImg2.copy())
+		tmpLbl = transforms.ToTensor()(tmpLbl.copy())
+
+		return {'imidx':torch.from_numpy(imidx), 'image': tmpImg, 'label': tmpLbl, 'LBP': tmpImg2}
